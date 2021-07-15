9fee7cabed9d0af196a49f193b4d831a2cb1bcc5
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2021-07-15 21:35:08 +0530

    Create ReverseString.java

---
+++ ReverseString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ReverseTheString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String initial, rev = "";
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the string to reverse");
+		initial = in.nextLine();
+		int length = initial.length();
+		for(int i=length-1;i>=0;i--)
+		  rev=rev+initial.charAt(i);
+		System.out.println("Reversed string: "+rev);
+		}
+		}
