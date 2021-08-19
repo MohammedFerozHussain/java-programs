e5e2e691497b05ab82248302dfa32cb0c580fe55
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2021-08-19 11:53:58 +0530

    Create Numbers.java

---
+++ Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num = sc.nextInt();
+		for(int i =1;i<=num;i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
