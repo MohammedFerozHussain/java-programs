d62fabf9d5d4298eed653133d42f31e32e9e1fd1
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2021-08-19 11:48:52 +0530

    Update ArrayDeclaration.java

--- ArrayDeclaration.java
+++ ArrayDeclaration.java
@@ -1,1 +1,20 @@
+import java.util.*;
+public class ArrayDeclaration {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Array Values are");
+		int a[] = new int[5];
+		a[0] = 1;
+		a[1] = 2;
+		a[2] = 4;
+		a[3] = 6;
+		a[4] = 5;
+		for(int i = 0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		
+	}
+
+}
