63ec6c4a4aa1f5e306dd6ffdcd706e5156050b3c
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2021-08-19 11:53:20 +0530

    Create LengthofString.java

---
+++ LengthofString.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class LengthOfTheString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String str = s.nextLine();
+		System.out.println("The Length of a given string :" +str.length());
+		
+		
+
+	}
+
+}
