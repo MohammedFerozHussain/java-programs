4db49e7d9165dfb2b8ea7f85877ce0ba591372bc
MohammedFerozHussain <46441228+MohammedFerozHussain@users.noreply.github.com>
2021-08-19 11:52:06 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,16 @@
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int year = 2000;
+		if(year % 4 == 0) {
+			System.out.println("Given Year is leap year");
+			}
+		else {
+			System.out.println("Given Year is Non-Leap Year");
+		}
+
+	}
+
+}
